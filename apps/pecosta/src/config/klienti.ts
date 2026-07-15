978df41dff020edb4da83ce18bfb4732bc15f6c2
Krystof Paleta <paletakrystof@gmail.com>
2026-07-15 15:51:33 +0200

    feat(pecosta): reálná klientská loga (40 gastro + 11 veřejný sektor) místo placeholderů; WebP v public/logos/klienti(-vs), config/klienti.ts; veřejný sektor na Burzovní obchody; smazány ref-*.svg

---
+++ apps/pecosta/src/config/klienti.ts
@@ -0,0 +1,64 @@
+/**
+ * Klientská loga Pecosty (reálné reference). Soubory: public/logos/klienti(-vs)/<slug>.webp.
+ * Názvy jsou vč. podsložky, aby je sdílený LogoWall poskládal jako /logos/<name>.webp.
+ * Řazení: silné/známé značky napřed (marquee), zbytek dál.
+ */
+
+// Gastro provozy, hotely, restaurace
+export const klientiGastro = [
+  'klienti/ambiente',
+  'klienti/cpi-hotels',
+  'klienti/pytloun-hotels',
+  'klienti/kolkovna',
+  'klienti/sasazu',
+  'klienti/grandhotel-pupp',
+  'klienti/hp-tronic',
+  'klienti/perfect-canteen',
+  'klienti/la-lorraine',
+  'klienti/kavarna-slavia',
+  'klienti/hotel-maximus',
+  'klienti/grandhotel-brno',
+  'klienti/avanti-hotel',
+  'klienti/jan-hotels',
+  'klienti/hh-hotels',
+  'klienti/con-gusto',
+  'klienti/la-fresca',
+  'klienti/mistral-cafe',
+  'klienti/pivo-karlin',
+  'klienti/goose-pivovar',
+  'klienti/mestansky-pivovar-turnov',
+  'klienti/restaurace-mincovna',
+  'klienti/restaurace-tiskarna',
+  'klienti/cacao',
+  'klienti/cerna-madona',
+  'klienti/knedlin',
+  'klienti/staromestska',
+  'klienti/u-pavouka',
+  'klienti/u-zlate-psenice',
+  'klienti/dolni-pocernice',
+  'klienti/heipark',
+  'klienti/cervenohorske-sedlo',
+  'klienti/hotel-freud',
+  'klienti/hotel-orlik',
+  'klienti/hotel-praded',
+  'klienti/hotel-radun',
+  'klienti/hotel-rott',
+  'klienti/hotel-rustikal',
+  'klienti/hotel-duo',
+  'klienti/hotel-obzor',
+];
+
+// Veřejný sektor (burzovní obchody / veřejné zakázky)
+export const klientiVerejnySektor = [
+  'klienti-vs/moravskoslezsky-kraj',
+  'klienti-vs/mesto-ostrava',
+  'klienti-vs/mesto-ricany',
+  'klienti-vs/mesto-dobris',
+  'klienti-vs/mesto-semily',
+  'klienti-vs/fakultni-nemocnice-ostrava',
+  'klienti-vs/nemocnice-frydek-mistek',
+  'klienti-vs/nemocnice-nymburk',
+  'klienti-vs/nemocnice-trinec',
+  'klienti-vs/lazne-karlova-studanka',
+  'klienti-vs/upmd',
+];
